0e6e65ff6e392b973268e1a94c2e5d615719b7f0
xinhao1231 <162409661+xinhao1231@users.noreply.github.com>
2026-06-25 00:21:20 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fangzhou-price-calculator-v23";
+const CACHE_NAME = "fangzhou-price-calculator-v24";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
