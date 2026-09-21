e9061d223ed671f20a278bb9de9bd6f89c088ed4
xinhao1231 <162409661+xinhao1231@users.noreply.github.com>
2026-09-21 16:27:20 +0800

    Add files via upload

--- chat-config.js
+++ chat-config.js
@@ -1,2 +1,2 @@
 // Public settings only. Never put API keys or passwords here.
-window.FANGZHOU_CHAT_CONFIG = Object.freeze({ apiBase: "" });
+window.FANGZHOU_CHAT_CONFIG = Object.freeze({ apiBase: "https://fangzhou-chat.xinhao-meta-leads.workers.dev" });
